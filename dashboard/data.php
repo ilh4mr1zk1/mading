a39e901162d967190b971cd8b83b3e03b2230a54
ilhamrizki <irp100202@gmail.com>
2024-01-19 09:37:41 +0700

    update fitur dashboard

--- dashboard/data.php
+++ dashboard/data.php
@@ -143,39 +143,6 @@
 		$output_all = '';
 		$allDataApprove = '';
 
-		$server 	= "localhost";
-		$username	= "root";
-		$password 	= "";
-		$database 	= "mading_practice";
-
-		$conn = mysqli_connect($server, $username, $password, $database);
-
-		$queryNya = "
-			SELECT message.id as message_id, message.message_title as judul_pesan, message.message_info as isi_pesan, message.status_approve as status_approve, message.user_id as user_id, users.id as id_users, users.nama_user as nama_user, users.email as email FROM message 
-            LEFT JOIN users
-            ON message.user_id = users.id
-            WHERE message.status_approve = 1 ";
-		
-		$result = mysqli_query($conn, $queryNya);
-
-		// while ($row = mysqli_fetch_array($result)) { 
-		// 	$outputNya .= 
-		// 	'
-		// 		<li class="show_data" data-toggle="modal" data-id="'. $row['message_id'] .'" data-from="'. $row['nama_user'] .'" data-title="'. $row['judul_pesan'] .'" data-main="'. $row['isi_pesan'] .'" data-target="modal-default">
-	    //               <a href="#">
-	    //                 <div class="pull-left">
-	    //                   <img src="../dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
-	    //                 </div>
-	    //                 <h4>
-	    //                   <p style="font-size:10px;"> From : '. $row['nama_user'] .' </p>
-	    //                   <p style="font-size:10px;"> Via<span style="margin-left: 11px;"></span>: Admin </p>
-	    //                 </h4>
-	    //                 <h4> '. $row['isi_pesan'] .' </h4>
-	    //               </a>
-	    //       	</li>
-	    //     ';
-	    // }
-
 		if (count($getShortDataNotifMessage) != 0) {
 
 		    for ($i=0; $i < count($getShortDataNotifMessage); $i++) {
